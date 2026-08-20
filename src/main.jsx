a8d6830ec1d996619860230fef4d98bf4e271875
Shashwat Ghadge <shxshwat23@gmail.com>
2026-08-20 14:29:10 +0530

    Add periodic trends to application

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import PageNavigation from './components/PageNavigation.jsx'
 import AtomBuilder from './components/AtomBuilder/AtomBuilder.jsx'
+import TrendsExplorer from './components/Trends/TrendsExplorer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <PageNavigation />
     <AtomBuilder />
+    <TrendsExplorer />
   </StrictMode>,
 )
