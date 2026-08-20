70ac91cde380c9d87bc680f0e835c7701c98c036
Shashwat Ghadge <shxshwat23@gmail.com>
2026-08-20 14:25:57 +0530

    Mount atom builder in application

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import PageNavigation from './components/PageNavigation.jsx'
+import AtomBuilder from './components/AtomBuilder/AtomBuilder.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <PageNavigation />
+    <AtomBuilder />
   </StrictMode>,
 )
