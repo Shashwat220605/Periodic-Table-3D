545229dae8d434ec32cb25a83c9f9f61afe84b48
Shashwat Ghadge <shxshwat23@gmail.com>
2026-08-20 14:21:19 +0530

    Connect page navigation to the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PageNavigation from './components/PageNavigation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <PageNavigation />
   </StrictMode>,
 )
